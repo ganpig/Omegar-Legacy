4c596ce64c812b7835d7d1c47353a3ddad0fd564
WERDXZ <lwerdxzl@hotmail.com>
2022-09-09 20:52:50 -0700

    autoCorrect

--- autoCorrect.py
+++ autoCorrect.py
@@ -1,19 +1,8 @@
-# By WERDXZ
-# On 2022-09-08
+from ctypes import *
 
 
 
-def autoCorrect(doubleList:list,precision:int=2,accuracy:int=50)->list:
-    """
-    Takes a list of doubles and returns a list of doubles with the same length
-    but with the values corrected to the nearest 0.5
-    @param doubleList: list of doubles
-    @param precision: number of decimal places to round to
-    @param accuracy: number of decimal places to check for accuracy
-    @return: list of doubles
-    """
-    deltaList=[]
-    for i in range(len(doubleList)):
-        deltaList.append(round(doubleList[i],precision)-doubleList[i])
-    
-    
+listToArray = lambda li: (c_double*len(li))(*li)
+
+
+arrayToList = lambda array,length: [array[i] for i in range   (0, length)]
